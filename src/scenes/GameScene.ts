4aae16d57b00283c133318729dbbf18b9bc2b7d6
Michael Auld <mauld@uci.edu>
2021-06-01 01:59:38 -0700

    Added movement - Added DEMO layer showing music buttons

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,9 +1,11 @@
 import Phaser from 'phaser'
 
 export default class GameScene extends Phaser.Scene {
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
+  private plink!: Phaser.Physics.Arcade.Sprite
 
   constructor() {
-    super('game-scene')
+    super('gamescene')
   }
 
   preload() {
@@ -11,6 +13,8 @@ export default class GameScene extends Phaser.Scene {
     this.load.tilemapTiledJSON('tilemap', 'assets/maps/stage.json')
 
     this.load.atlas('plink', 'assets/char/plink.png', 'assets/char/plink.json')
+
+    this.cursors = this.input.keyboard.createCursorKeys()
   }
 
   create() {
@@ -21,25 +25,88 @@ export default class GameScene extends Phaser.Scene {
     const tileset = map.addTilesetImage('tiles', 'spritesheet')
 
     map.createLayer('background', tileset)
+    map.createLayer('DEMO', tileset)
     const foregroundLayer = map.createLayer('foreground', tileset)
 
     foregroundLayer.setCollisionByProperty({ collides: true });
 
-    const debugGraphics = this.add.graphics().setAlpha(0.75);
-    map.renderDebug(debugGraphics, {
-      tileColor: null, // Color of non-colliding tiles
-      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-      faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-    });
+    // const debugGraphics = this.add.graphics().setAlpha(0.75);
+    // map.renderDebug(debugGraphics, {
+    //   tileColor: null, // Color of non-colliding tiles
+    //   collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+    //   faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+    // });
 
-    const spawnPoint = map.findObject("objects", obj => obj.name === "spawn");
+    //const spawnPoint = map.findObject("objects", obj => obj.name === "spawn");
 
-    const plink = this.add.sprite(spawnPoint.x, spawnPoint.y, 'plink', 'char_front.png')
+    this.plink = this.physics.add.sprite(119, 217, 'plink', 'char_front.png')
 
-    this.physics.add.collider(plink, foregroundLayer);
+    this.anims.create({
+      key: "plink_up_idle",
+      frames: [{ key: 'plink', frame: 'char_back.png' }]
+    })
+
+    this.anims.create({
+      key: "plink_down_idle",
+      frames: [{ key: 'plink', frame: 'char_front.png' }]
+    })
+
+    this.anims.create({
+      key: "plink_up",
+      frames: [{ key: 'plink', frame: 'char_back.png' }] //TO ANIMATE
+    })
+
+    this.anims.create({
+      key: "plink_down",
+      frames: [{ key: 'plink', frame: 'char_front.png' }] //TO ANIMATE
+    })
+
+    this.anims.create({
+      key: "plink_left",
+      frames: [{ key: 'plink', frame: 'char_left.png' }] //TO ANIMATE
+    })
+
+    this.anims.create({
+      key: "plink_left_idle",
+      frames: [{ key: 'plink', frame: 'char_left.png' }]
+    })
+
+    this.anims.create({
+      key: "plink_right",
+      frames: [{ key: 'plink', frame: 'char_right.png' }] //TO ANIMATE
+    })
+
+    this.anims.create({
+      key: "plink_right_idle",
+      frames: [{ key: 'plink', frame: 'char_right.png' }]
+    })
+
+    this.plink.anims.play('plink_down_idle')
+
+    this.physics.add.collider(this.plink, foregroundLayer);
   }
 
-  update() {
+  update(t, dt) {
+    if (!this.cursors || !this.plink) {
+      return
+    }
 
+    const speed = 80;
+
+    if (this.cursors.left?.isDown) {
+      this.plink.anims.play('plink_left')
+      this.plink.setVelocity(-speed, 0)
+    } else if (this.cursors.right?.isDown) {
+      this.plink.anims.play('plink_right')
+      this.plink.setVelocity(speed, 0)
+    } else if (this.cursors.up?.isDown) {
+      this.plink.anims.play('plink_up')
+      this.plink.setVelocity(0, -speed)
+    } else if (this.cursors.down?.isDown) {
+      this.plink.anims.play('plink_down')
+      this.plink.setVelocity(0, speed)
+    } else {
+      this.plink.setVelocity(0, 0)
+    }
   }
 }
