c1e0acf44921f2e3a7871e15d9a769b0cade1234
Michael Auld <mauld@uci.edu>
2021-06-06 19:12:56 -0700

    Added button detection and removed button collision

--- src/items/Button.ts
+++ src/items/Button.ts
@@ -2,12 +2,11 @@ import Phaser from 'phaser'
 
 export default class Button extends Phaser.Physics.Arcade.Sprite {
 
-  private _note;
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number) {
     super(scene, x, y, texture, frame)
   }
 
   press() {
-
+    console.log(this.name + " button pressed!")
   }
 }
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -39,23 +39,23 @@ export default class GameScene extends Phaser.Scene {
     objectLayer.objects.forEach(obj => {
       if (obj.type === 'button') {
         if (obj.name === 'play') {
-          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'notePlay', 'notePlay.png')
+          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'notePlay', 'notePlay.png').setName('play')
         } else if (obj.name === 'noteRed') {
-          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteRed', 'noteRed.png')
+          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteRed', 'noteRed.png').setName('red')
         } else if (obj.name === 'noteOrange') {
-          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteOrange', 'noteOrange.png')
+          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteOrange', 'noteOrange.png').setName('orange')
         } else if (obj.name === 'noteYellow') {
-          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteYellow', 'noteYellow.png')
+          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteYellow', 'noteYellow.png').setName('yellow')
         } else if (obj.name === 'noteGreen') {
-          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteGreen', 'noteGreen.png')
+          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteGreen', 'noteGreen.png').setName('green')
         } else if (obj.name === 'noteCyan') {
-          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteCyan', 'noteCyan.png')
+          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteCyan', 'noteCyan.png').setName('cyan')
         } else if (obj.name === 'noteBlue') {
-          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteBlue', 'noteBlue.png')
+          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'noteBlue', 'noteBlue.png').setName('blue')
         } else if (obj.name === 'notePurple') {
-          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'notePurple', 'notePurple.png')
+          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'notePurple', 'notePurple.png').setName('purple')
         } else if (obj.name === 'notePink') {
-          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'notePink', 'notePink.png')
+          buttons.get(obj.x! + obj.width! * 0.5, obj.y! - obj.height! * 0.5, 'notePink', 'notePink.png').setName('pink')
         }
       } else if (obj.type === 'door') {
 
@@ -71,12 +71,12 @@ export default class GameScene extends Phaser.Scene {
     debugDraw(foregroundLayer, this);
 
     this.physics.add.collider(this.plink, foregroundLayer);
-    this.physics.add.collider(this.plink, buttons, this.handlePlayerButtonCollision, undefined, this);
+    this.physics.add.overlap(this.plink, buttons, this.handlePlayerButtonCollision, undefined, this);
   }
 
   private handlePlayerButtonCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
     const button = obj2 as Button
-
+    button.press()
   }
 
   update(t, dt) {
