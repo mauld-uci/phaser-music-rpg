9960d69263a6f1c2b4bfe0df69698ab590c41a3c
Michael Auld <mauld@uci.edu>
2021-06-06 20:32:20 -0700

    Mapped MIDI numbers to Buttons

--- src/items/Button.ts
+++ src/items/Button.ts
@@ -7,6 +7,44 @@ export default class Button extends Phaser.Physics.Arcade.Sprite {
   }
 
   press() {
-    console.log(this.name + " button pressed!")
+    let note = -1;
+    switch (this.name) {
+      case 'red': {
+        note = 60 //C
+        break
+      }
+      case 'orange': {
+        note = 62 //D
+        break
+      }
+      case 'yellow': {
+        note = 64 //E
+        break
+      }
+      case 'green': {
+        note = 65 //F
+        break
+      }
+      case 'cyan': {
+        note = 67 //G
+        break
+      }
+      case 'blue': {
+        note = 69 //A
+        break
+      }
+      case 'purple': {
+        note = 71 //B
+        break
+      }
+      case 'pink': {
+        note = 72 //C 
+        break
+      }
+      default: {
+        break
+      }
+    }
+    console.log(note + " button pressed!")
   }
 }
