9fc3a004aff563325c79fac06b5ee4cedab3a551
Michael Auld <mauld@uci.edu>
2021-06-07 13:40:07 -0700

    Added note button functionality

--- src/items/Button.ts
+++ src/items/Button.ts
@@ -8,6 +8,7 @@ export default class Button extends Phaser.Physics.Arcade.Sprite {
 
   private context
   private oscillator
+  private isStarted = false;
 
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number) {
     super(scene, x, y, texture, frame)
@@ -18,41 +19,49 @@ export default class Button extends Phaser.Physics.Arcade.Sprite {
     switch (this.name) {
       case 'red': {
         note = 60 //C
+        this.playSound(note)
         this.anims.play('red_pressed')
         break
       }
       case 'orange': {
         note = 62 //D
+        this.playSound(note)
         this.anims.play('orange_pressed')
         break
       }
       case 'yellow': {
         note = 64 //E
+        this.playSound(note)
         this.anims.play('yellow_pressed')
         break
       }
       case 'green': {
         note = 65 //F
+        this.playSound(note)
         this.anims.play('green_pressed')
         break
       }
       case 'cyan': {
         note = 67 //G
+        this.playSound(note)
         this.anims.play('cyan_pressed')
         break
       }
       case 'blue': {
         note = 69 //A
+        this.playSound(note)
         this.anims.play('blue_pressed')
         break
       }
       case 'purple': {
         note = 71 //B
+        this.playSound(note)
         this.anims.play('purple_pressed')
         break
       }
       case 'pink': {
         note = 72 //C 
+        this.playSound(note)
         this.anims.play('pink_pressed')
         break
       }
@@ -83,7 +92,21 @@ export default class Button extends Phaser.Physics.Arcade.Sprite {
   }
 
   public playSound(value: number) {
+    this.noteOn(value)
+    this.scene.time.delayedCall(300, this.noteOff, [], this);
+  }
 
+  private noteOn(MIDI: number) {
+    this.getOrCreateContext()
+    this.oscillator.connect(this.context.destination);
+    const freq = Math.pow(2, (MIDI - 69) / 12) * 440
+
+    this.oscillator.frequency.setTargetAtTime(freq, this.context.currentTime, 0);
+    this.context.resume();
+  }
+
+  private noteOff() {
+    this.context.suspend();
   }
 
   public playTune(tune: Array<number>) {
@@ -94,8 +117,6 @@ export default class Button extends Phaser.Physics.Arcade.Sprite {
     for (var i: number = 0; i < tune.length; ++i) {
       pairs.push([tune[i], 4])
     }
-
-    console.log(pairs)
 
     this.getOrCreateContext()
     this.oscillator.connect(this.context.destination);
